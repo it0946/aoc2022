596bd789c3bc3fa39f5cdbd2a5cf606f155a2deb
it0946 <it@000964@gmail.com>
2022-12-01 09:52:28 +0200

    day1

---
+++ README.md
@@ -0,0 +1,2 @@
+## aoc2022
+My advent of code 2022 solutions. I probably won't be able to finish them all, but I will try. The language may change from day to day.
---
+++ day1.cpp
@@ -0,0 +1,43 @@
+#include <string>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+#include <fmt/core.h>
+
+std::string get_input()
+{
+    std::ifstream file{"inputs/day1"};
+    std::istreambuf_iterator<char> begin{file}, end;
+    return std::string{begin, end};
+}
+
+int solve(const std::string& input)
+{
+    std::vector<int> res{0};
+    size_t i = 0, pos = 0, prev = 0;
+
+    while ((pos = input.find('\n', prev)) != input.npos) {
+        std::string curr = input.substr(prev, pos - prev);
+
+        if (!curr.empty()) {
+            res[i] += std::stol(curr);
+        } else {
+            i += 1;
+            res.push_back(0);
+        }
+
+        prev = pos + 1;
+    }
+
+    std::sort(res.begin(), res.end());
+
+    size_t l = res.size();
+    fmt::print("part1: {}\npart2: {}\n", res.back(), res[l - 1] + res[l - 2] + res[l - 3]);
+
+    return 0;
+}
+
+int main()
+{
+    solve(get_input());
+}
